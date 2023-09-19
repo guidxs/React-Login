3636f63446e9a072f1cf27109ec9dbc16225dcbb
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-19 11:41:26 -0300

    v1.0

---
+++ src/components/ListaProdutos.jsx
@@ -0,0 +1,8 @@
+export const ListaProdutos =[
+
+    {id: 1, nome: 'Teclado Gamer', desc: 'Mecanico', preco: 200},
+    {id: 2, nome: 'Mouse Gamer', desc: 'Mouse', preco: 300},
+    {id: 3, nome: 'Cadeira Gamer', desc: 'Top', preco: 2200},
+    {id: 4, nome: 'phone Gamer', desc: 'Melhor Som', preco: 1500},
+    {id: 5, nome: 'Mousepad Gamer', desc: 'Plastico', preco: 100}
+]
